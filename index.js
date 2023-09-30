38faf3b02e300972373f1247899c45e710946643
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-09-30 12:28:10 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left);
+    left -= 10; // Adjust the amount you want to move the DODGER to the left.
+    dodger.style.left = `${left}px`;
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left);
+    left += 10; // Adjust the amount you want to move the DODGER to the right.
+    dodger.style.left = `${left}px`;
+  }
